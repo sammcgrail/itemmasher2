6bdf5bc3a114bea9281599788f4b330ef2854b97
sammcgrail <sammcgrail@gmail.com>
2017-04-22 14:06:17 -0400

    init

---
+++ logic.js
@@ -0,0 +1,15 @@
+console.log("JS File loaded")
+
+function imgRedirect(destination){
+
+ location.href = destination;
+
+}
+
+function pullStats(imagenumber){
+
+ console.log("item stats")
+ console.log("file name id number of item " + imagenumber)
+ console.log( "Limited to 1.\nUNIQUE Passive: Enhanced Movement: +25 Movement Speed")
+
+}
