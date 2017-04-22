4855c393d0cbb5a106a6f764bba303f10423e26d
sammcgrail <sammcgrail@gmail.com>
2017-04-22 14:16:50 -0400

    logic begin

--- logic.js
+++ logic.js
@@ -7,9 +7,29 @@ function imgRedirect(destination){
 }
 
 function pullStats(imagenumber){
+  var dictionary =
+  {
+  	"id": 1001,
+  	"name": "Boots of Speed",
+    "cost": 300,
+    "stats": [0,0,0,0,0,0,25,0,0,0,0,0,0,0,0,0,0,0,0],
+  	"desc": "Limited to 1.\nUNIQUE Passive: Enhanced Movement: +25 Movement Speed",
+  }
+  //
+  // {
+  // 	"id": 2,
+  // 	"name": "fairie charm",
+  //   "cost": 125,
+  //   "stats": [0,0,0,0,0,25,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  // 	"desc": "Gives mana regen",
+  // },
+  console.log(imagenumber)
+  console.log("item stats")
+  // console.log(dictionary.id)
+  // console.log(dictionary.name)
+  // console.log(item.id)
 
- console.log("item stats")
- console.log("file name id number of item " + imagenumber)
- console.log( "Limited to 1.\nUNIQUE Passive: Enhanced Movement: +25 Movement Speed")
+ // console.log("file name id number of item " + imagenumber)
+ // console.log( "Limited to 1.\nUNIQUE Passive: Enhanced Movement: +25 Movement Speed")
 
 }
